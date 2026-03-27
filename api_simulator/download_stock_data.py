4212e7399de8b73662fa917af6915f348371630b
Archit <karchit@umich.edu>
2026-03-27 16:21:59 -0400

    added a script that downloads a week of minute by minute stock data for a couple handpicked stocks. We can adjust this but this is a good baseline

---
+++ api_simulator/download_stock_data.py
@@ -0,0 +1,59 @@
+"""
+Download relevant stock data for the API caching experiment using the yfinance library.
+
+See this for downloading data syntax:
+https://ranaroussi.github.io/yfinance/reference/api/yfinance.download.html
+
+See this for how to get the data via the .history call:
+https://ranaroussi.github.io/yfinance/reference/api/yfinance.Tickers.html
+
+
+To limit data overload, we only download a small set of carefully chosen tickers rather than a huge universe of stock. 
+
+The goal is not to model the full market, but rather to gather enough realistic price series to simulate mock API calls under different cache policies.
+
+In particular, I've chosen a mix of common, recognizable stocks and more volatile stocks (to increase the chance that stale cached prices actually influence correctness/staleness/
+
+The downloaded data will be stored locally and later replayed by the mock API simulator.
+
+This is important because the actual experiment should not depend on live API/library calls as external factors may introduce untwanted noise/latency.
+
+Later, the mock API simulator will replay this saved data as if it were serving real stock-price requests.
+
+"""
+
+
+
+# Common Recoganizable Stocks: , Google (GOOG), Tesla (TSLA), Nvidia (NVDA), Meta (META)
+
+# More volatile stocks: Coinbase (COIN), Mara Holdings (MARA), Hut 8 (HUT), SoundHound AI (SOUN), Riot Platforms (RIOT), IREN Limited (IREN)
+
+
+
+from __future__ import annotations
+
+from datetime import datetime, timedelta, timezone
+
+import yfinance as yf
+
+
+
+
+tickers = ['GOOG', 'TSLA', 'NVDA', 'META', 'COIN', 'MARA', 'HUT', 'SOUN', 'RIOT', 'IREN']
+
+end = datetime.now(timezone.utc)
+start = end - timedelta(days=10)
+
+df = yf.download(
+    tickers=tickers,
+    period = "7d",
+    interval="1m",
+    group_by="ticker",
+    auto_adjust=False,
+    actions=False,
+    prepost=False,
+)
+
+open_df = df.xs("Open", axis=1, level=1)
+open_df.to_csv("stocks_open_only.csv")
+print(open_df.head())
